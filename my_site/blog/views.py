252723a5517ec687edd8bdb96531d268b07d02c1
nikocer96 <nikocerone96@gmail.com>
2025-01-21 19:29:48 +0100

    Commit added

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def starting_page(request):
-    return HttpResponse("ciao")
+    return render(request, "blog/index.html")
 
 def posts(request):
     pass
